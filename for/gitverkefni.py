de87327d8280ea257e7ec3ecb9b3cfb11142d371
Kormakur Atli <kormakurun285@nemi.tskoli.is>
2017-01-23 16:53:29 +0000

    Gitverkefni - valmynd

--- for/gitverkefni.py
+++ for/gitverkefni.py
@@ -1,1 +1,15 @@
 #Gitverkefni
+
+#Valmynd
+#Valmynd
+valmynd = 0
+while valmynd != 4:
+    print("*---------- VALMYND ----------*")
+    print("1 = Summa og margföldun")
+    print("2 = Nafn")
+    print("3 = Hástafir og lágstafir")
+    print("4 = Hætta")
+    print(" ")
+    # Notandi slær inn valmöguleikann sem hann vill
+    valmynd = int(input("Sláðu inn valmöguleikann sem þú vilt: "))
+    print(" ")
